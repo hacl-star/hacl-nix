79f80ca990c24cb77ce0c6b6f49d1ed2988091c9
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-11-09 08:00:00 +0100

    import hacl

--- flake.nix
+++ flake.nix
@@ -12,9 +12,19 @@
     };
     flake-utils.url = "flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    hacl = {
+      url = "github:hacl-star/hacl-star";
+      inputs = {
+        fstar-src.follows = "fstar-src";
+        karamel-src.follows = "karamel-src";
+        hacl-nix.follows = "/";
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, fstar-src, karamel-src, flake-utils, nixpkgs }:
+  outputs = { self, fstar-src, karamel-src, flake-utils, nixpkgs, hacl }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -22,8 +32,9 @@
           mapAttrs mapAttrs' mapAttrsToList nameValuePair filterAttrs foldAttrs;
         haclDeps =
           import ./haclDeps.nix { inherit pkgs fstar-src karamel-src; };
+        haclPackages = haclDeps // { inherit (hacl.packages.${system}) hacl; };
       in rec {
-        packages = haclDeps;
-        hydraJobs = haclDeps;
+        packages = haclPackages // { default = haclPackages.hacl; };
+        hydraJobs = haclPackages;
       });
 }
