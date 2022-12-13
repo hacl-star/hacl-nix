7c446259548fffd64711c1ceb3646796eb0ba876
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-12-13 08:00:00 +0100

    update fstar build
    Co-authored-by: Lucas Franceschino <lucas@franceschino.fr>

--- haclDeps.nix
+++ haclDeps.nix
@@ -2,16 +2,16 @@
 
 let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
-  haclDeps = rec {
-    z3 = pkgs.callPackage ./z3 { };
-    fstar = pkgs.callPackage ./fstar {
-      inherit ocamlPackages z3;
-      src = fstar-src;
-    };
-    karamel = pkgs.callPackage ./karamel {
-      inherit ocamlPackages fstar z3;
-      src = karamel-src;
-    };
-    vale = pkgs.callPackage ./vale { };
+  z3 = pkgs.callPackage ./z3 { };
+  vale = pkgs.callPackage ./vale { };
+  libFstar = pkgs.callPackage ./fstar/lib.nix { inherit ocamlPackages z3; };
+  fstar = libFstar.binary-of-ml-snapshot {
+    pname = "fstar";
+    version = fstar-src.rev;
+    src = fstar-src;
   };
-in haclDeps
+  karamel = pkgs.callPackage ./karamel {
+    inherit ocamlPackages fstar z3;
+    src = karamel-src;
+  };
+in { inherit z3 vale fstar karamel; }
