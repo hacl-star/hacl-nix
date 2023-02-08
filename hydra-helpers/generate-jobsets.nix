cd9ad65490d60e4e5ee35595cf4ea17798f6bb81
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-02-08 16:17:44 +0100

    remove hydra-helpers

--- hydra-helpers/generate-jobsets.nix
+++
@@ -1,4 +0,0 @@
-{ owner, repo, prs, refs, hacl-nix, ... }:
-(import (hacl-nix + "/hydra-helpers/default.nix")).lib.${builtins.currentSystem}.makeGitHubJobsets
-  {inherit owner repo;}
-  {inherit prs refs;}
